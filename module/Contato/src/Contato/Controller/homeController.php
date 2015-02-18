191d437a895b88affe5533ec726eea24cdd48ad3
Marcelo <Marcelo@MFS>
2015-02-18 00:53:57 -0200

    terminado brach Model

--- module/Contato/src/Contato/Controller/homeController.php
+++ module/Contato/src/Contato/Controller/homeController.php
@@ -9,10 +9,10 @@ use Zend\View\Model\ViewModel;
  
 class HomeController extends AbstractActionController
 {
-    /**
-     * action index
-     * @return \Zend\View\Model\ViewModel
-     */
+   /**
+    * action index
+    * @return \Zend\View\Model\ViewModel
+    */
     public function indexAction()
     {
         return new ViewModel();
